bfcb530287244201331100508407330fae826e29
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2020-08-04 20:09:06 -0300

    chore(#11): add debug in create-table script
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- scripts/create-tables.sh
+++ scripts/create-tables.sh
@@ -2,7 +2,12 @@
 
 . env/.aws.env
 
+set -x
+
 for t in json/*_table.json; do
+  echo "$REGION"
+  echo "file://$t"
+  echo "ENDPOINT"
 	aws dynamodb create-table \
 		--region "$REGION" \
 		--cli-input-json "file://$t" \
