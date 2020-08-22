7cccc4d474f07d38ed7b94f70e26dfc7cb4622b5
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2020-08-22 18:46:19 -0300

    refactor(#48): rename internal package to pkg
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- pkg/infra/awscli/repository.go
+++ pkg/infra/awscli/repository.go
@@ -1,7 +1,7 @@
 package awscli
 
 import (
-	"aws-poc/internal/infra"
+	"aws-poc/pkg/infra"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
