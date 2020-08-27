0e82fa0d7fab70307138cc4381a9828235fbae8c
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2020-08-27 20:35:32 -0300

    fix(#54): extract handle to a function avoiding race conditions
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- pkg/infra/awscli/concurpoller.go
+++ pkg/infra/awscli/concurpoller.go
@@ -15,14 +15,8 @@ func (p concurPoller) run(c consumer, h handler, messages []*sqs.Message) {
 
 	wg.Add(numMessages)
 	for i := range messages {
-		go func(m *sqs.Message) {
-			defer wg.Done()
-			if err := p.handleMessage(c, h, m); err != nil {
-				log.Println(err)
-			}
-		}(messages[i])
+		go handle(p, c, h, &wg, messages[i])
 	}
-
 	wg.Wait()
 }
 
@@ -31,4 +25,11 @@ func (p concurPoller) handleMessage(c consumer, h handler, m *sqs.Message) error
 		return err
 	}
 	return c.deleteMessage(m.ReceiptHandle)
+}
+
+func handle(p concurPoller, c consumer, h handler, wg *sync.WaitGroup, m *sqs.Message) {
+	defer wg.Done()
+	if err := p.handleMessage(c, h, m); err != nil {
+		log.Println(err)
+	}
 }
