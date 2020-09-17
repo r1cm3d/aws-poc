821f2a33b287b7e0c8717f2d7731d3322a2e6e22
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2020-09-17 07:34:55 -0300

    fix(#53): adjust configuration files
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- pkg/awscli/s3_test.go
+++ pkg/awscli/s3_test.go
@@ -12,7 +12,7 @@ import (
 const (
 	bucketName = "bucket"
 	key        = "kellyKey"
-	filename   = "../../../scripts/env/.env"
+	filename   = "../../scripts/env/.env"
 )
 
 func TestUploadIntegration(t *testing.T) {
@@ -67,7 +67,7 @@ func TestGetIntegration(t *testing.T) {
 	setupBucket()
 	defer cleanupBucket()
 
-	file, err := os.Open("../../../scripts/env/.env")
+	file, err := os.Open("../../scripts/env/.env")
 	if err != nil {
 		log.Fatal("enable to open file")
 	}
