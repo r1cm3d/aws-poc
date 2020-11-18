711be84b6c7768fab5f5f66683762dfa3d24cd2d
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2020-11-18 20:20:39 -0300

    chore(#89): remove unused dynamodb tables and replace them for one domain table
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- deployments/terraform/dynamodb.tf
+++ deployments/terraform/dynamodb.tf
@@ -1,40 +1,13 @@
 resource "aws_dynamodb_table" "chargeback-table" {
-  name         = "ChargebackError"
+  name         = "Dispute"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "Dispute_ID"
-  range_key    = "TimeStamp"
+  hash_key     = "ID"
 
   attribute {
-    name = "Dispute_ID"
-    type = "N"
-  }
-
-  attribute {
-    name = "TimeStamp"
+    name = "ID"
     type = "S"
   }
-
-  tags = {
-    Environment = var.account
-  }
-}
-
-resource "aws_dynamodb_table" "claim-table" {
-  name         = "Claim"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "Dispute_ID"
-  range_key    = "TimeStamp"
-
-  attribute {
-    name = "Dispute_ID"
-    type = "N"
-  }
-
-  attribute {
-    name = "TimeStamp"
-    type = "S"
-  }
-
+  
   tags = {
     Environment = var.account
   }
